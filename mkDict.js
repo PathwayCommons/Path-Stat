4126c3952e7e269e1fb30656b42b60e490c26091
Dylan Fong <d2fong@uwaterloo.ca>
2017-03-20 16:28:48 -0400

    simplifies code

--- mkDict.js
+++ mkDict.js
@@ -1,16 +1,16 @@
-var Promise = require('bluebird');
-var lodash = require('lodash');
-var fs = require('fs');
-var fsp = require('fs-promise');
-var fetch = require('node-fetch');
+const Promise = require('bluebird');
+const pick = require('lodash.pick');
+const fs = require('fs');
+const fsp = require('fs-promise');
+const fetch = require('node-fetch');
 fetch.Promise = Promise;
 
-var sbgnConverter = require('sbgnml-to-cytoscape');
+const sbgnConverter = require('sbgnml-to-cytoscape');
 
-var baseUrlSearch = 'http://beta.pathwaycommons.org/pc2/search.json?q=*&type=pathway';
-var baseUrlGet = 'http://beta.pathwaycommons.org/pc2/get?format=sbgn&uri=';
+const baseUrlSearch = 'http://beta.pathwaycommons.org/pc2/search.json?q=*&type=pathway';
+const baseUrlGet = 'http://beta.pathwaycommons.org/pc2/get?format=sbgn&uri=';
 
-var getNumPages = fetch(baseUrlSearch)
+const getNumPages = fetch(baseUrlSearch)
   .then(result => result.json())
   .then(resultObj => {
     return Math.floor((resultObj.numHits - 1) / resultObj.maxHitsPerPage) + 1;
@@ -19,9 +19,9 @@ var getNumPages = fetch(baseUrlSearch)
 function fetchSearch(baseUrl, pageNumber) {
   // Some code taken from fetch-retry
   return new Promise(function(resolve, reject) {
-    var wrappedFetch = function(n) {
-      if (n >= 0) {
-        fetch(baseUrlSearch + '&page=' + pageNumber)
+    var wrappedFetch = function(numTries) {
+      if (numTries >= 0) {
+        fetch(`${baseUrlSearch}&page=${pageNumber}`)
           .then(res => res.json())
           .then(searchObj => {
             if (typeof searchObj === 'object') {
@@ -32,7 +32,7 @@ function fetchSearch(baseUrl, pageNumber) {
           })
           .then(resolvable => resolve(resolvable))
           .catch((e) => {
-            wrappedFetch(--n);
+            wrappedFetch(--numTries);
           });
       }
     };
@@ -42,8 +42,8 @@ function fetchSearch(baseUrl, pageNumber) {
 function fetchGet(uri) {
   // Some code taken from fetch-retry
   return new Promise(function(resolve, reject) {
-    var wrappedFetch = function(n) {
-      if (n >= 0) {
+    var wrappedFetch = function(numTries) {
+      if (numTries >= 0) {
         fetch(baseUrlGet + uri)
           .then(res => res.text())
           .then(searchStr => {
@@ -51,7 +51,7 @@ function fetchGet(uri) {
           })
           .then(resolvable => resolve(resolvable))
           .catch((e) => {
-            wrappedFetch(--n);
+            wrappedFetch(--numTries);
           });
       }
     };
@@ -59,83 +59,70 @@ function fetchGet(uri) {
   });
 }
 var pathway_array = getNumPages
-  .then(numPages => {
-    return Array(numPages).fill(0).map(function(x, i) {
-      return i;
-    });
-  })
-  .map(pageNumber => {
-    return fetchSearch(baseUrlSearch, pageNumber);
-  }, {
-    concurrency: 6
-  })
-  .then(arrayList => {
-    var output = [];
-    for (var i = 0; i < arrayList.length; i++) {
-      output = output.concat(arrayList[i]);
-    }
-    console.log(output);
-    return output;
-  })
+  .then(numPages => [...Array(numPages).keys()])
+  .map(pageNumber => fetchSearch(baseUrlSearch, pageNumber))
+  .then(arrayList => [].concat(...arrayList))
   .catch((e) => console.log(e));
 
-// Use pathway_array to generate object where the key are pathways and the values are number of referenced symbols
-var pathway_list = pathway_array.then(pathwayObject => {
-  return pathwayObject.map(pathway => {
-    return lodash.pick(pathway, ['name', 'size', 'dataSource']);
-  });
-});
+pathway_array.then(output => console.log(output));
 
-// Use pathway_array to generate object where the key are pathways and the values are number of referenced symbols
-var uri_list = pathway_array.then(pathwayObject => {
-     return pathwayObject.map(pathway => {
-     return lodash.pick(pathway, ['uri']);
-   });
-  })
-  .map(uri => {
-    console.log(uri);
-    return fetchGet(uri.uri).then( jsonData => {
-    console.log('START');
-    console.log(jsonData);
-    return {
-      numNodes: jsonData.nodes.length,
-      numEdges: jsonData.edges.length,
-      numClassCompartment: jsonData.map(node => node.data.class === 'compartment' ? 1 : 0).reduce((acc, cur) => acc + cur),
-      compartmentLabels: jsonData.map(node => node.data.class === 'compartment' ? node.data.label : null),
-      numChildNodes: jsonData.map(node => node.data.parent !== '' ? 1 : 0).reduce((acc, cur) => acc + cur)
-    };
-  })}, {
-    concurrency: 12
-  });
-
-Promise.all([pathway_array, pathway_list]).then(promiseArray => {
-  console.log('');
-  console.log('Processing completed');
-  console.log(Object.keys(promiseArray[1]).length + ' pathways found');
-  console.log('');
-});
-
-function writeToFile(file_name, output) {
-  var path = __dirname + '/output/' + file_name;
-  try {
-    fs.mkdirSync(__dirname + '/output');
-  } catch (e) {}
-  fsp.remove(path + '.min.json').then(() => {
-    fsp.writeFile(path + '.min.json', JSON.stringify(output));
-  });
-  fsp.remove(path + '.json').then(() => {
-    fsp.writeFile(path + '.json', JSON.stringify(output, null, 2));
-  });
-}
-
-// Write pathway_array to file
-pathway_array.then(output => {
-  var file_name = "pathway_array";
-  writeToFile(file_name, output);
-});
-
-// Write pathway_weighted to file
-pathway_list.then(output => {
-  var file_name = "pathway_list";
-  writeToFile(file_name, output);
-});
+// // Use pathway_array to generate object where the key are pathways and the values are number of referenced symbols
+// var pathway_list = pathway_array.then(pathwayObject => {
+//   return pathwayObject.map(pathway => {
+//     return lodash.pick(pathway, ['name', 'size', 'dataSource']);
+//   });
+// });
+//
+// // Use pathway_array to generate object where the key are pathways and the values are number of referenced symbols
+// var uri_list = pathway_array.then(pathwayObject => {
+//      return pathwayObject.map(pathway => {
+//      return lodash.pick(pathway, ['uri']);
+//    });
+//   })
+//   .map(uri => {
+//     console.log(uri);
+//     return fetchGet(uri.uri).then( jsonData => {
+//     console.log('START');
+//     console.log(jsonData);
+//     return {
+//       numNodes: jsonData.nodes.length,
+//       numEdges: jsonData.edges.length,
+//       numClassCompartment: jsonData.map(node => node.data.class === 'compartment' ? 1 : 0).reduce((acc, cur) => acc + cur),
+//       compartmentLabels: jsonData.map(node => node.data.class === 'compartment' ? node.data.label : null),
+//       numChildNodes: jsonData.map(node => node.data.parent !== '' ? 1 : 0).reduce((acc, cur) => acc + cur)
+//     };
+//   })}, {
+//     concurrency: 12
+//   });
+//
+// Promise.all([pathway_array, pathway_list]).then(promiseArray => {
+//   console.log('');
+//   console.log('Processing completed');
+//   console.log(Object.keys(promiseArray[1]).length + ' pathways found');
+//   console.log('');
+// });
+//
+// function writeToFile(file_name, output) {
+//   var path = __dirname + '/output/' + file_name;
+//   try {
+//     fs.mkdirSync(__dirname + '/output');
+//   } catch (e) {}
+//   fsp.remove(path + '.min.json').then(() => {
+//     fsp.writeFile(path + '.min.json', JSON.stringify(output));
+//   });
+//   fsp.remove(path + '.json').then(() => {
+//     fsp.writeFile(path + '.json', JSON.stringify(output, null, 2));
+//   });
+// }
+//
+// // Write pathway_array to file
+// pathway_array.then(output => {
+//   var file_name = "pathway_array";
+//   writeToFile(file_name, output);
+// });
+//
+// // Write pathway_weighted to file
+// pathway_list.then(output => {
+//   var file_name = "pathway_list";
+//   writeToFile(file_name, output);
+// });

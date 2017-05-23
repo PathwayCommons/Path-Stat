dfbbb94d808ca17ae77f4001f068cb2fc9f1d2ae
Dylan Fong <d2fong@uwaterloo.ca>
2017-05-23 13:04:59 -0400

    cytosnap + batch compute layout WIP

--- src/batchComputeLayout.js
+++ src/batchComputeLayout.js
@@ -1,106 +1,73 @@
 const fs = require('fs');
 const path = require('path');
 
-// const cytosnap = require('cytosnap');
-// const SBGNRenderer = require('sbgn-renderer');
-// const coseBilkent = require('cytoscape-cose-bilkent');
-
-// coseBilkent( SBGNRenderer.__proto__ );
-
+const Promise = require('bluebird');
+const cytosnap = require('cytosnap');
 
 const baseDir = path.resolve('.');           // make it a command line opt
 const sbgnDir = baseDir + '/sbgn/';          // make it a command line opt
 const sbgnJSONDir = baseDir + '/sbgnjson/';  // make it a command line opt
 const coseTransformDir = baseDir + '/cose_bilkent_transform/';
 
-// const files = fs.readdirSync(sbgnDir);
+const files = fs.readdirSync(sbgnDir).filter((file) => file !== '.DS_Store');
 
-// const renderer = new SBGNRenderer();
+const snap = cytosnap();
 
-// const snap = cytosnap();
 
-// for (let fname of files) {
-//   const sbgnJSON = JSON.parse(fs.readFileSync(sbgnJSONDir + fname + '.json', 'utf-8'));
-//   console.log(sbgnJSON);
-//   const num_nodes = sbgnJSON.nodes.length;
-//   // skip empty files and record
-//   if (num_nodes === 0) {
-//     continue;
-//   }
-
-//   if (num_nodes < 350) {
-//     continue;
-//   }
-
-//   // renderer.remove('*');
-//   // renderer.add(sbgnJSON);
-//   // renderer.layout({
-//   //   name: 'grid',
-//   //   stop: function () {
-//   //     fs.writeFile(coseTransformDir + fname + '.json', renderer.json(), (err) => {
-//   //       if (err) throw err;
-//   //     });
-//   //   }
-//   // });
-
-//   snap.start().then(function(){
-//     return snap.shot({
-//       elements: sbgnJSON,
-//       layout: {
-//         name: 'grid',
-//       },
-//       resolvesTo: 'json'
-//     });
-//   }).then(function(img) {
-//     console.log(img);
-//     fs.writeFile(coseTransformDir + fname + '.json', img, (err) => {
-//       if (err) throw err;
-//     });
-//   });
-
-// }
-
-var cytosnap = require('cytosnap');
-var snap = cytosnap();
-
-snap.start().then(function(){
-  return snap.shot({
-    elements: [ // http://js.cytoscape.org/#notation/elements-json
-      { data: { id: 'foo' } },
-      { data: { id: 'bar' } },
-      { data: { source: 'foo', target: 'bar' } }
-    ],
-    layout: { // http://js.cytoscape.org/#init-opts/layout
-      name: 'grid'
-    },
-    style: [ // http://js.cytoscape.org/#style
-      {
-        selector: 'node',
-        style: {
-          'background-color': 'red'
-        }
+const processFile = (json) => {
+  return new Promise((resolve, reject) => {
+    return snap.shot({
+      elements: json,
+      layout: { // http://js.cytoscape.org/#init-opts/layout
+        name: 'grid'
       },
-      {
-        selector: 'edge',
-        style: {
-          'line-color': 'red'
+      style: [ // http://js.cytoscape.org/#style
+        {
+          selector: 'node',
+          style: {
+            'background-color': 'red'
+          }
+        },
+        {
+          selector: 'edge',
+          style: {
+            'line-color': 'red'
+          }
         }
-      }
-    ],
-    resolvesTo: 'base64uri',
-    format: 'png',
-    width: 640,
-    height: 480,
-    background: 'transparent'
+      ],
+      resolvesTo: 'base64uri',
+      format: 'png',
+      width: 640,
+      height: 480,
+      background: 'transparent'
+    })
+    .then((img) => {
+      // do whatever you want with img
+      console.log(img);
+      resolve(img);
+    })
+    .catch((e) => {
+      reject(e);
+    });
   });
-}).then(function( img ){
-  // do whatever you want with img
-  fs.writeFile(coseTransformDir  + '1.png', img, (err) => {
-    if (err) throw err;
+};
+
+snap.start().then(function () {
+  const processedImgs = files.map((f) => {
+    const json = JSON.parse(fs.readFileSync(sbgnJSONDir + f + '.json',  'utf-8'));
+    return processFile(json);
   });
 
-  console.log( img );
-  snap.stop();
+  // Promise.all(processedImgs)
+  // .then(function(out) {
+  //   console.log(out);
+  //   snap.stop();
+  // });
+  // const json = JSON.parse(fs.readFileSync(sbgnJSONDir + files[0] + '.json', 'utf-8'));
+  // const result = processFile(json);
+  // result.then((output) => {
+  //   console.log(output);
+  //   snap.stop();
+  // });
 });
-
 
